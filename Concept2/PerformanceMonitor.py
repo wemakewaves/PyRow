e4a5c0ef3693f108992660f7354623620624c0ea
Vijendra Patel <vj@uvd.co.uk>
2015-07-01 18:00:25 +0100

    WIP need to add set workout to PM

--- Concept2/PerformanceMonitor.py
+++ Concept2/PerformanceMonitor.py
@@ -6,6 +6,9 @@ __author__ = 'UVD'
 
 import usb.util
 import time
+from threading import Lock
+from PyRow import csafe_cmd
+from PyRow.Concept2.Response import Response
 
 
 class PerformanceMonitor(object):
@@ -70,11 +73,37 @@ class PerformanceMonitor(object):
     }
 
     MIN_FRAME_GAP = .050
+    TIMEOUT = 2000
 
+    STROKE_WAIT_MIN_SPEED = 0
+    STROKE_WAIT_FOR_ACCELERATION = 1
+    STROKE_DRIVE = 2
+    STROKE_DWELLING = 3
+    STROKE_RECOVERY = 4
+
+    STATE_ERROR = 0
+    STATE_READY = 1
+    STATE_IDLE = 2
+    STATE_HAVE_ID = 3
+    STATE_NA = 4
+    STATE_IN_USE = 5
+    STATE_PAUSE = 6
+    STATE_FINISHED = 7
+    STATE_MANUAL = 8
+    STATE_OFFLINE = 9
+
+    GET_STATUS = 'CSAFE_GETSTATUS_CMD'
     GET_TIME = 'CSAFE_PM_GET_WORKTIME'
     GET_DISTANCE = 'CSAFE_PM_GET_WORKDISTANCE'
     GET_CADENCE = 'CSAFE_PM_GETCADENCE_CMD'
-    GET_POWER = 'CSAFE_PM_GETCADENCE_CMD'
+    GET_POWER = 'CSAFE_GETPOWER_CMD'
+    GET_STROKE_STATE = 'CSAFE_PM_GET_STROKESTATE'
+    GET_PACE = 'CSAFE_GETPACE_CMD'
+    GET_CALORIES = 'CSAFE_GETCALORIES_CMD'
+
+    GO_FINISHED = 'CSAFE_GOFINISHED_CMD'
+    GO_IDLE = 'CSAFE_GOIDLE_CMD'
+    GO_READY = 'CSAFE_GOREADY_CMD'
 
     def __init__(self, device):
         """
@@ -82,13 +111,16 @@ class PerformanceMonitor(object):
         :return:
         """
         self.__device = device
-        print vars(device)
+        interface = device[0][(0, 0)]
+        self.__in_address = interface[0].bEndpointAddress
+        self.__out_address = interface[1].bEndpointAddress
 
         self.__manufacturer = usb.util.get_string(self.__device, self.__device.iManufacturer)
         self.__product = usb.util.get_string(self.__device, self.__device.iProduct)
         self.__serial_number = usb.util.get_string(self.__device, self.__device.iSerialNumber)
 
         self.__last_message = time.time()
+        self.__lock = Lock()
 
     def get_manufacturer(self):
         """
@@ -113,3 +145,66 @@ class PerformanceMonitor(object):
         :return string:
         """
         return PerformanceMonitor.PM_VERSION[self.__device.idProduct]
+
+    def send_commands(self, commands):
+        """
+        :param [] commands:
+        :return Response:
+        """
+        self.__lock.acquire()
+        now = time.time()
+        delta = now - self.__last_message
+        deltaraw = delta.seconds + delta.microseconds/1000000.
+        if deltaraw < PerformanceMonitor.MIN_FRAME_GAP:
+            time.sleep(PerformanceMonitor.MIN_FRAME_GAP - deltaraw)
+
+        csafe = csafe_cmd.write(commands)
+
+        length = self.__device.write(self.__out_address, csafe, timeout=PerformanceMonitor.TIMEOUT)
+        self.__last_message = time.time()
+
+        response = []
+        while not response:
+            try:
+                transmission = self.__device.read(self.__in_address, length, timeout=2000)
+                response = csafe_cmd.read(transmission)
+            except Exception as e:
+                raise e
+
+        time.sleep(PerformanceMonitor.MIN_FRAME_GAP)
+        self.__lock.release()
+
+        return Response(response)
+
+    def get_status(self):
+        """
+        Gets the current status from the Performance Monitor
+        :return Response:
+        """
+        return self.send_commands([
+            PerformanceMonitor.GET_STATUS
+        ])
+
+    def reset(self):
+        """
+        Resets the Performance Monitor or throws an Exception if unable to
+        :return:
+        """
+        state = self.get_status()
+        print "Current Status: {0}".format(state.get_status_message())
+
+        manual = state.get_status() == PerformanceMonitor.STATE_MANUAL
+        offline = state.get_status() == PerformanceMonitor.STATE_OFFLINE
+
+        if manual or offline:
+            # TODO: Make Exception class
+            raise Exception("Ergometer is in: {0}".format(state.get_status_message()))
+
+        finished = state.get_status() == PerformanceMonitor.STATE_FINISHED
+        ready = state.get_status() == PerformanceMonitor.STATE_READY
+
+        if not finished and not ready:
+            self.send_commands([PerformanceMonitor.GO_FINISHED])
+
+        self.send_commands([PerformanceMonitor.GO_IDLE])
+        self.send_commands([PerformanceMonitor.GO_READY])
